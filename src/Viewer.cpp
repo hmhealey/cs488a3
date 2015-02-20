6fc6f18743e12fbc4a8cfbd6a9328e6216d86952
hmhealey <harrisonmhealey@gmail.com>
2015-02-20 16:55:16 -0500

    That last commit was actually to remove the old Mesh class and rename NMesh to Mesh

--- src/Viewer.cpp
+++ src/Viewer.cpp
@@ -116,8 +116,8 @@ void Viewer::paintGL() {
     program.setUniformValue("modelView", toQt(Matrix4::makeTranslation(0, 0, -2) * Matrix4::makeRotation(0, angle, 0)));
     program.setUniformValue("modelViewProjection", toQt(Matrix4::makePerspective(30, 1, 0.1, 10) * Matrix4::makeTranslation(0, 0, -2) * Matrix4::makeRotation(0, angle, 0)));
 
-    //Mesh* mesh = Mesh::makeBox(1, 1, 1);
-    Mesh* mesh = Mesh::makeIcosphere(0.5f, 1);
+    Mesh* mesh = Mesh::makeBox(1, 1, 1);
+    //Mesh* mesh = Mesh::makeIcosphere(0.5f, 1);
     mesh->draw(program);
     delete mesh;
 
