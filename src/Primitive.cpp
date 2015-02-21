0ce4694e1f24dcca341f1b46f77a980e500d687b
hmhealey <harrisonmhealey@gmail.com>
2015-02-21 15:28:24 -0500

    Attached a mesh and a material to each Primitive object

--- src/Primitive.cpp
+++ src/Primitive.cpp
@@ -1,15 +1,25 @@
 #include "Primitive.hpp"
 
+#include <QGLShaderProgram>
+
+#include "Algebra.hpp"
 #include "Mesh.hpp"
+#include "Shader.hpp"
+#include "Viewer.hpp"
 
 Primitive::~Primitive() { }
 
-Sphere::Sphere(double radius) { }
+void Primitive::setMaterial(const PhongMaterial& material) {
+    this->material = material;
+}
+
+Sphere::Sphere(double radius) : mesh(Mesh::makeCube(1)) { } // TODO this shouldn't be a cube
 
 Sphere::~Sphere() {
     delete mesh;
 }
 
-void Sphere::walk_gl(bool picking) const {
-    // Fill me in
+void Sphere::draw(Shader& shader, bool picking) const {
+    shader.setMaterial(material);
+    mesh->draw(shader);
 }
--- src/Primitive.hpp
+++ src/Primitive.hpp
@@ -1,22 +1,32 @@
 #ifndef CS488_PRIMITIVE_HPP
 #define CS488_PRIMITIVE_HPP
 
-#include "Algebra.hpp"
+#include "Material.hpp"
 
+struct Matrix4;
 class Mesh;
+class Shader;
 
 class Primitive {
+protected:
+    PhongMaterial material;
+    
 public:
     virtual ~Primitive();
-    virtual void walk_gl(bool picking) const = 0;
+
+    void setMaterial(const PhongMaterial& material);
+
+    virtual void draw(Shader& shader, bool picking = false) const = 0;
 };
 
 class Sphere : public Primitive {
     Mesh* mesh;
+
 public:
     Sphere(double radius = 1.0);
     virtual ~Sphere();
-    virtual void walk_gl(bool picking) const;
+
+    virtual void draw(Shader& shader, bool picking = false) const;
 };
 
 #endif
